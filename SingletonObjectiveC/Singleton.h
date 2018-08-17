3ac5bd44f80abbc7bf4268b961adab43ffd75904
kishanrcs <kishan.ramisetty@gmail.com>
2018-08-17 10:53:36 +0530

    First Commit with no data

---
+++ SingletonObjectiveC/Singleton.h
@@ -0,0 +1,13 @@
+//
+//  Singleton.h
+//  SingletonObjectiveC
+//
+//  Created by MI on 17/08/18.
+//  Copyright © 2018 MAGNAHEALTHSOLUTIONS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Singleton : NSObject
+
+@end
---
+++ SingletonObjectiveC/Singleton.m
@@ -0,0 +1,13 @@
+//
+//  Singleton.m
+//  SingletonObjectiveC
+//
+//  Created by MI on 17/08/18.
+//  Copyright © 2018 MAGNAHEALTHSOLUTIONS. All rights reserved.
+//
+
+#import "Singleton.h"
+
+@implementation Singleton
+
+@end
